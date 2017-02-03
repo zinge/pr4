97b666c96116e58262e9a6ecc4db5d207a8a6d2c
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-02-03 23:16:15 +0300

    Changes to be committed: 	modified:   database/migrations/2017_02_02_202632_create_mvzs_table.php

--- database/migrations/2017_02_02_202632_create_mvzs_table.php
+++ database/migrations/2017_02_02_202632_create_mvzs_table.php
@@ -15,7 +15,7 @@ class CreateMvzsTable extends Migration
     {
         Schema::create('mvzs', function (Blueprint $table) {
             $table->increments('id');
-            $table->int('mvz_cod');
+            $table->integer('mvz_cod');
             $table->char('mvz_desc',128);
             $table->timestamps();
         });
