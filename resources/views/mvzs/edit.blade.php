f8c2596b127ab38c9b9741c756b25a0e8d271d76
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-02-07 23:21:14 +0300

    Changes to be committed: 	modified:   resources/views/mvzs/edit.blade.php

--- resources/views/mvzs/edit.blade.php
+++ resources/views/mvzs/edit.blade.php
@@ -2,5 +2,27 @@
 
 @section('content')
   <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <div class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Описание</th>
+                <th>Код</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{{ $mvz->id }}</td>
+                <td>{{ $mvz->mvz_desc }}</td>
+                <td>{{ $mvz->mvz_cod }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 @endsection
