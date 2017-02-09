6a1a9c5fbb3b56760ed77c519d917781e69c65aa
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-02-10 00:46:14 +0300

    Changes to be committed: 	modified:   config/app.php 	new file:   resources/views/common/errors.blade.php 	modified:   resources/views/home.blade.php 	modified:   resources/views/layouts/app.blade.php 	renamed:    resources/views/layouts/app.blade.php -> resources/views/layouts/app.blade.php.orig 	new file:   resources/views/layouts/footer.blade.php 	new file:   resources/views/layouts/header.blade.php 	new file:   resources/views/layouts/menu.blade.php 	new file:   resources/views/layouts/styles/default.blade.php 	new file:   resources/views/mvzs/edit.blade.php 	new file:   resources/views/mvzs/index.blade.php 	modified:   resources/views/welcome.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    С корабля Земля казалась сверкающим серпом, лежащим далеко внизу.
                 </div>
             </div>
         </div>
