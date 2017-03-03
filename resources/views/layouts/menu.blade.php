9b96b40f36eb17ee92f985a90b24daefa4cb6ce4
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-03-03 19:07:44 +0300

    Changes to be committed: 	modified:   resources/views/cost/index.blade.php 	modified:   resources/views/layouts/menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -41,9 +41,9 @@
                     @if (Auth::user()->hasRole(['admins', 'root']))
                       <ul class="nav navbar-nav">
                           <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Файлы <span class="caret"></span></a>
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Загрузки<span class="caret"></span></a>
                               <ul class="dropdown-menu">
-                                <li><a href="{{ url('/fu') }}">Загрузить</a></li>
+                                <li><a href="{{ url('/fu') }}">Файл</a></li>
                               </ul>
                           </li>
                       </ul>
