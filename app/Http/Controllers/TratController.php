8fdc221abc576977bd8b8f3a3704465b19217b8c
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-03-18 10:51:23 +0300

    Changes to be committed: 	new file:   app/Http/Controllers/TratController.php 	new file:   app/Trat.php 	new file:   database/migrations/2017_03_18_063157_create_trats_table.php

---
+++ app/Http/Controllers/TratController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Trat;
+use Illuminate\Http\Request;
+
+class TratController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Trat  $trat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trat $trat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Trat  $trat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trat $trat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Trat  $trat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trat $trat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Trat  $trat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trat $trat)
+    {
+        //
+    }
+}
